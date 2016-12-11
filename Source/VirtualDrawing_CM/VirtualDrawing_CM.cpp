6c57803960bcf8e7f445dceffe59e269ba3f3b8a
cwmorse <cwm83@cornell.edu>
2016-12-11 17:27:18 -0500

    Add files via upload

---
+++ Source/VirtualDrawing_CM/VirtualDrawing_CM.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VirtualDrawing_CM.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VirtualDrawing_CM, "VirtualDrawing_CM" );
